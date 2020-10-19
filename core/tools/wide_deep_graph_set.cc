de55da3dca0946cc60096428e74473280f5b8fb2
yaolei <yaolei@360.cn>
2020-10-19 18:27:43 +0800

    [opt] remove objectpool

--- core/tools/wide_deep_graph_set.cc
+++ core/tools/wide_deep_graph_set.cc
@@ -1,7 +1,6 @@
 #include "core/tools/graph.h"
 
 #include "core/tools/graph_base.h"
-#include "core/tools/object_pool.h"
 
 #include "tensorflow/core/platform/logging.h"
 
@@ -10,6 +9,7 @@
 #include <set>
 #include <iomanip>
 #include <fstream>
+#include <thread>
 
 #include <boost/algorithm/string.hpp>
 
@@ -65,71 +65,52 @@ private:
     std::map<std::string, std::string> _table;
 };
 
-class GraphObj {
+class WideDeepGraph : public GraphBase {
 public:
-    Graph* GetGraph() {
-        return &_graph;
-    }
+    WideDeepGraph() {}
 
-    GraphObj(const std::string& path) {
-        std::string var_params_file = path + "/dense_params.txt";
-        std::ifstream var_params_if(var_params_file);
+    virtual ~WideDeepGraph() {}
+
+    int InitDenseParams(const std::string& dense_params_file) {
+        std::ifstream var_params_if(dense_params_file);
         if (!var_params_if.is_open()) {
-            LOG(ERROR) << "open " << var_params_file << " failed.";
-            return;
+            LOG(ERROR) << "open " << dense_params_file << " failed.";
+            return -1;
         }
+
+        Graph graph;
 
         std::string input;
         while(getline(var_params_if, input)) {
             std::vector<std::string> var_params_vec;
             boost::algorithm::split(var_params_vec, input, boost::is_any_of("\t"));
             if (var_params_vec.size() != 2) {
-                LOG(ERROR) << "var_params_file format error.";
-                return;
+                LOG(ERROR) << "dense_params_file format error.";
+                return -1;
             }
             std::string var_params_name = var_params_vec[0];
             boost::trim(var_params_name);
             std::vector<std::string> var_params_weight_strs;
             boost::algorithm::split(var_params_weight_strs, var_params_vec[1], boost::is_any_of("\1"));
 
-            int index = _graph.LookupVariableIndex(var_params_name);
-            size_t var_size = _graph.arg_size(index);
+            int index = graph.LookupVariableIndex(var_params_name);
+            size_t var_size = graph.arg_size(index);
 
-            //LOG(INFO) << "arg name:" << var_params_name <<", var size:" << var_size;
             if (var_size != var_params_weight_strs.size() * sizeof(float)) {
                 LOG(ERROR) << "var param " << var_params_name << " size is " << var_params_weight_strs.size()
                            << ", expected " << var_size / sizeof(float);
-                return;
+                return -1;
             }
             std::vector<float> var_params_weight;
             for (auto& w : var_params_weight_strs) {
                 var_params_weight.push_back(std::stof(w));
             }
-            std::copy(var_params_weight.begin(), var_params_weight.end(), static_cast<float*>(_graph.arg_data(index)));
-
-            //std::string weight_str = "";
-            //for (size_t z = 0; z < var_params_weight.size(); ++z) {
-            //    weight_str += "," + std::to_string((static_cast<float*>(_graph.arg_data(index))[z]));
-            //}
+            _dense_params.insert({index, std::move(var_params_weight)});
         }
+
+        return 0;
     }
 
-private:
-    Graph _graph;
-};
-
-class WideDeepGraph : public GraphBase {
-public:
-    WideDeepGraph() {}
-
-    virtual ~WideDeepGraph() {
-        if (_graph_pool != NULL) {
-            delete _graph_pool;
-            _graph_pool = NULL;
-        }
-    }
-
-    // @param arg_type_info : feed_wide_emb, feed_deep_emb, feed_deep_lr_emb
     int Init(const char* conf_file) {
         ConfParser conf;
         if (conf.Init(conf_file) < 0) {
@@ -150,19 +131,11 @@ public:
             return -1;
         }
 
-        std::string pool_size_str;
-        int pool_size = 1;
-        if (conf.Get("pool_size", pool_size_str) < 0) {
-            LOG(ERROR) << "conf get item pool_size failed.";
-            return -1;
-        }
-
         try {
             _max_batch_size = std::stoi(max_batch_size);
             slot_num = std::stoi(slot_num_str);
-            pool_size = std::stoi(pool_size_str);
         } catch(...) {
-            LOG(ERROR) << "conf max_batch_size or slot_num or pool_size is not int.";
+            LOG(ERROR) << "conf max_batch_size or slot_num is not int.";
             return -1;
         }
 
@@ -204,9 +177,6 @@ public:
             feed_names_index_map.insert({s, index});
 
             auto ret = arg_types_set.insert(s.substr(0, s.rfind("_")));
-            if (ret.second) {
-                _emb_dims.push_back(graph.arg_size(index) / _max_batch_size / sizeof(float));
-            }
         }
 
         if (arg_types_vec.size() != arg_types_set.size()) {
@@ -214,6 +184,15 @@ public:
             return -1;
         }
         _arg_type_num = arg_types_set.size();
+
+        for (auto& s : arg_types_vec) {
+            int index = graph.LookupArgIndex(s + "_0");
+            if (index < 0) {
+                LOG(ERROR) << "arg not found:" << s + "_0";
+                return -1;
+            }
+            _emb_dims.push_back(graph.arg_size(index) / _max_batch_size / sizeof(float));
+        }
 
         for (int i = 0; i < slot_num; ++i) {
             for (int t = 0; t < _arg_type_num; ++t) {
@@ -226,7 +205,11 @@ public:
             }
         }
 
-        _graph_pool = new ObjectPool<GraphObj>(pool_size, path);
+        std::string dense_params_file = path + "/dense_params.txt";
+        if (InitDenseParams(dense_params_file) < 0) {
+            LOG(ERROR) << "InitDenseParams Error, file:" << dense_params_file;
+            return -1;
+        }
 
         return 0;
     }
@@ -238,16 +221,10 @@ public:
         int cursor = 0;
         int last_cursor = 0;
 
-        GraphObj* graph_obj = GetGraphObj();
-        if (graph_obj == NULL) {
-            LOG(ERROR) << "GetGraphObj returns NULL ptr.";
-            return -1;
-        }
+        Graph graph;
 
-        Graph* graph = graph_obj->GetGraph();
-        if (graph == NULL) {
-            LOG(ERROR) << "GetGraph returns NULL ptr.";
-            return -1;
+        for (auto& p : _dense_params) {
+            std::copy(p.second.begin(), p.second.end(), static_cast<float*>(graph.arg_data(p.first)));
         }
 
         for (size_t i = 0; i < input.size(); ++i) {
@@ -269,42 +246,41 @@ public:
                     int end = start + dim;
 
                     int index = GetIndex(s, t);
-                    //std::cerr << "index:" << index << std::endl;
                     if (index < 0) {
                         LOG(ERROR) << "GetIndex error.";
                         return -1;
                     }
 
                     std::copy(input[i][s].fm_array.data() + start, input[i][s].fm_array.data() + end,
-                              static_cast<float*>(graph->arg_data(index)) + ((cursor - last_cursor - 1) * dim));
+                              static_cast<float*>(graph.arg_data(index)) + ((cursor - last_cursor - 1) * dim));
 
                     start = end;
                 }
             }
 
             if (cursor % _max_batch_size == 0) {
-                auto ok = graph->Run();
+                auto ok = graph.Run();
                 if (!ok) {
                     LOG(ERROR) << "graph run failed.";
                     return -1;
                 }
 
-                std::copy(graph->result0_data(),
-                          graph->result0_data() + cursor - last_cursor,
+                std::copy(graph.result0_data(),
+                          graph.result0_data() + cursor - last_cursor,
                           output.data() + last_cursor);
                 last_cursor = cursor;
             }
         }
 
         if (last_cursor != cursor) {
-            auto ok = graph->Run();
+            auto ok = graph.Run();
             if (!ok) {
                 LOG(ERROR) << "graph run failed.";
                 return -1;
             }
 
-            std::copy(graph->result0_data(),
-                      graph->result0_data() + cursor - last_cursor,
+            std::copy(graph.result0_data(),
+                      graph.result0_data() + cursor - last_cursor,
                       output.data() + last_cursor);
         }
 
@@ -312,9 +288,6 @@ public:
             LOG(ERROR) << "graph run output size not correct.";
             return -1;
         }
-
-
-        ReturnGraph(graph_obj);
 
         return 0;
     }
@@ -347,26 +320,12 @@ public:
         return _emb_dims[arg_type];
     }
 
-    GraphObj* GetGraphObj() {
-        GraphObj* obj = _graph_pool->GetObject();
-        if (obj == NULL) {
-            LOG(ERROR) << "GetGraph failed.";
-            return NULL;
-        }
-
-        return obj;
-    }
-
-    void ReturnGraph(GraphObj* obj) {
-        _graph_pool->ReturnObject(obj);
-    }
-
 private:
     int _max_batch_size;
     int _arg_type_num;
     std::vector<int> _emb_dims;
     std::vector<int> _slot_index;
-    ObjectPool<GraphObj>* _graph_pool;
+    std::map<int, std::vector<float> > _dense_params;
 };
 
 }; // namespace tf_serving
